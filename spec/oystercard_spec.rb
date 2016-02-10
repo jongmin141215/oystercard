45a3a9ed30115e114dda55f3865376097df38198
Jongmin Kim <kjm141215@gmail.com>
2016-02-10 12:17:38 -0800

    Add top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,13 @@
 require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { Oystercard.new }
-  it 'has balance of 0 when created' do
+  it 'has a balance of 0 when created' do
     expect(oystercard.balance).to eq 0
+  end
+
+  it 'can top up money' do
+    oystercard.top_up(10)
+    expect{ oystercard.top_up 10 }.to change{ oystercard.balance }.by 10
+    # expect(oystercard.balance).to eq 10
   end
 end
